d64d581eba2d18595bac5308054c99e07cbe5936
f-zach <63615390+f-zach@users.noreply.github.com>
2022-08-22 11:12:32 +0200

    added funtion to send data to the controller

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,20 @@
 #include <TCO.h>
 #include <FRQ.h>
 
-
 #define LED_ERR 5
 #define LED_BUSY 6
 
-MAINmodule MAIN(24,0x76);
-ADCmodule ADC(0x49,0x48);
+MAINmodule MAIN(24, 0x76);
+ADCmodule ADC(0x49, 0x48);
 TCOmodule TCO(0x20);
 FRQmodule FRQ(115200);
 
 void readGoPowerBox();
-//Konstruktoren für alle Module
+// Konstruktoren für alle Module
 
-long t, tMeasurementStart;
+long t, tMeasurementStart, rpmBrake, tLastSentCont = 0, tLastSentPC = 0;
 int val, sensor_cs, i;
-float frequnecy, dataGoPowerBox[3];
+float frequnecy, torque, power;
 char command;
 bool fault;
 
@@ -28,60 +27,67 @@ void setup()
 {
   Serial.begin(9600);
   Serial4.begin(9600);
+  Serial5.begin(115200);
 
   Serial.println("starting");
   pinMode(LED_ERR, OUTPUT);
   pinMode(LED_BUSY, OUTPUT);
 
-  digitalWrite(LED_BUSY,HIGH);
+  digitalWrite(LED_BUSY, HIGH);
 
   delay(5000);
-  
+
   Wire.begin(I2C_MASTER, 0x00, I2C_PINS_18_19);
 
   SPI.begin();
 
   MAIN.config();
-  
+
   ADC.config(B00000111);
 
-  TCO.config(B01010100, 1);
+  TCO.config(B00000111, 1);
 
- digitalWrite(LED_BUSY,LOW);
+  digitalWrite(LED_BUSY, LOW);
 }
 
 void loop()
 {
-  //Lese Befehlssymbol
+  digitalWriteFast(LED_BUSY, HIGH);
+
+  MAIN.startTmeasurement();
+
+  ADC.readAll();
+
+  TCO.readTempAll();
+
+  FRQ.getFrequency(1);
+
+  MAIN.readEnvP();
+  MAIN.readEnvT();
+
+  readGoPowerBox();
+  // Lese Befehlssymbol
   if (Serial.available())
   {
     command = Serial.read();
   }
 
-  //Switch zur ausführung der Befehle
+  if (millis() - tLastSentCont > 100)
+  {
+    Serial5.print("*r" + String(FRQ.frequency1) + ";s" + String(rpmBrake) + ";p" + String(power) + ";#");
+    // Serial.println("*r" + String(FRQ.frequency1) + ";s" + String(dataGoPowerBox[0]) + ";p" + String(dataGoPowerBox[2]) + ";#");
+    tLastSentCont = millis();
+  }
+
+  // Switch zur ausführung der Befehle
   switch (command)
   {
-  //Case 'r': Lese alle Module aus
+  // Case 'r': Lese alle Module aus
   case 'r':
-    digitalWriteFast(LED_BUSY, HIGH);
-
-    MAIN.startTmeasurement();
-
-    ADC.readAll();
-
-    TCO.readTempAll();
-
-    FRQ.getFrequency(1);
-
-    MAIN.readEnvP();
-    MAIN.readEnvT();
-
-    readGoPowerBox();
-
-    //Baue String für die Ausgabe über Serial auf
+    // Baue String für die Ausgabe über Serial auf
     for (i = 0; i < ADC.channelCount; i++)
     {
-      Serial.print(ADC.Voltage[i],5);
+      Serial.print(ADC.Voltage[i], 5);
       Serial.print("\t");
     }
 
@@ -94,14 +100,17 @@ void loop()
     Serial.print(FRQ.frequency1);
     Serial.print("\t");
 
-    Serial.print(FRQ.frequency2);
+    // Serial.print(FRQ.frequency2);
+    // Serial.print("\t");
+
+    Serial.print(rpmBrake);
     Serial.print("\t");
 
-    for(int i = 0; i<3; i++)
-    {
-      Serial.print(dataGoPowerBox[i]);
-      Serial.print("\t");
-    }
+    Serial.print(torque);
+    Serial.print("\t");
+
+    Serial.print(power);
+    Serial.print("\t");
 
     Serial.print(MAIN.envPressure);
     Serial.print("\t");
@@ -114,37 +123,80 @@ void loop()
     command = 0;
     digitalWriteFast(LED_BUSY, LOW);
     break;
+  }
 
+  if (millis() - tLastSentPC >= 100)
+  {
+    // Baue String für die Ausgabe über Serial auf
+    for (i = 0; i < ADC.channelCount; i++)
+    {
+      Serial.print(ADC.Voltage[i], 5);
+      Serial.print("\t");
+    }
+
+    for (i = 0; i < TCO.sensorCount; i++)
+    {
+      Serial.print(TCO.TemperatureC[i]);
+      Serial.print("\t");
+    }
+
+    Serial.print(FRQ.frequency1);
+    Serial.print("\t");
+
+    // Serial.print(FRQ.frequency2);
+    // Serial.print("\t");
+
+    Serial.print(rpmBrake);
+    Serial.print("\t");
+
+    Serial.print(torque);
+    Serial.print("\t");
+
+    Serial.print(power);
+    Serial.print("\t");
+
+    Serial.print(MAIN.envPressure);
+    Serial.print("\t");
+
+    Serial.print(MAIN.envTemperature);
+    Serial.print("\t");
+
+    Serial.println();
+
+    command = 0;
+
+    tLastSentPC = millis();
+
+    digitalWriteFast(LED_BUSY, LOW);
   }
 }
 
 void readGoPowerBox()
 {
-    if (Serial4.available()>10 && Serial4.available() < 24)
+  if (Serial4.available() > 10 && Serial4.available() < 24)
+  {
+    // Serial.println("Data Recieved");
+    for (int i = 0; i < 3; i++)
     {
-      //Serial.println("Data Recieved");
-        Serial4.read();
-        for(int i = 0; i < 3; i++)
-        {
-          String recieveString = Serial4.readStringUntil('\t');
-          dataGoPowerBox[i] = recieveString.toFloat();
-        }
-        
-    } 
-    else if(Serial4.available() >= 24)
-    {
-      //Serial.println(Serial4.available());
-      //Serial.println("Nothing recieved");
-
-      for(i = 0; i<64; i++)
-      {
       Serial4.read();
-      }
-
-      dataGoPowerBox[2] = 1;
     }
-    else
-    {
+    rpmBrake = Serial4.readStringUntil('\t').toInt();
+    torque = Serial4.readStringUntil('\t').toFloat();
+    power = Serial4.readStringUntil('\t').toFloat();
 
-    }    
+    // Serial.println(String(rpmBrake) + "\t" + String(torque) + "\t" + String(power));
+  }
+  else if (Serial4.available() >= 24)
+  {
+    // Serial.println(Serial4.available());
+    // Serial.println("Nothing recieved");
+
+    for (i = 0; i < 64; i++)
+    {
+      Serial4.read();
+    }
+  }
+  else
+  {
+  }
 }
